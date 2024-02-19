424c8c6d9bef3e46815912093313725778b9bed8
jarod-sys <142030523+jarod-sys@users.noreply.github.com>
2024-02-19 12:50:20 +0100

    soumission

--- soumission.sh
+++ soumission.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=Qrcode
-#SBATCH --array=1-10
-#SBATCH --time=00:15:30 # days-hh-mm-ss
-#SBATCH --mem-per-gpu=512 #megabytes
+# Submission script for Hercules2
+#SBATCH --job-name=MachineLearnSD
+#SBATCH --time=05:00:00 # hh:mm:ss
+#
+#SBATCH --ntasks=1
+#SBATCH --gres="gpu:4"
+#SBATCH --mem-per-cpu=1000 # megabytes
+#SBATCH --partition=gpu
+#
+#SBATCH --mail-user=jarod.ketchakouakep@student.unamur.be
+#SBATCH --mail-type=ALL
+#
+#SBATCH --output=outfile
 
-ml spider  Python
-ml spider  matplotlib
-ml spider tensorflow
+module purge 
+ml  releases/2022b TensorFlow/2.13.0-foss-2022b
+ml Python
+ml matplotlib
+
 
 echo "Task_ID : $SLURM_ARRAY_TASK_ID"
 python TrialAndError.py $SLURM_ARRAY_TASK_ID
